12b89b2ca41e510e116209da4b463f65c25ee015
Jorge David Mares Sánchez <Jorge.David.Mares@ibm.com>
2026-02-24 14:39:53 -0600

    First commit

---
+++ src/Controller/LandingPageController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Doctrine\ORM\Query\Expr\Func;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LandingPageController extends AbstractController
+{
+    public function __construct()
+    {
+
+    }
+
+    public function getLandingPage (){
+        
+    }
+}
---
+++ src/Service/MessageService.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Service;
+
+class MessageService
+{
+    public function __construct()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function getMessageResponse() {}
+}
